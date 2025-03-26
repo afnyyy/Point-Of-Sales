f941cb830f41464c504272ee33e69af3e4739487
afnyyy <afny27@gmail.com>
2025-03-26 14:24:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BelajarController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('belajar', [BelajarController::class, 'index']);
+Route::get('tambah', [BelajarController::class, 'tambah']);
+// Route::get('kurang', [BelajarController::class, 'kurang']);
+// Route::get('bagi', [BelajarController::class, 'bagi']);
+// Route::get('kali', [BelajarController::class, 'kali']);
+
+Route::post('action-tambah', [BelajarController::class, 'actionTambah']);
+Route::get('login', [LoginController::class,'login']);
+Route::post('action-login', [LoginController::class, 'actionLogin']);
